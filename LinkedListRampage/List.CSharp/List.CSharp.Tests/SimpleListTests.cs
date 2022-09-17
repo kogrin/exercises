8a8437e5373cea7c17b0e7815ba35c65d2d8a4d3
kogrin <kogron99@gmail.com>
2022-09-18 02:08:14 +0300

    Added out of range exception test

--- LinkedListRampage/List.CSharp/List.CSharp.Tests/SimpleListTests.cs
+++ LinkedListRampage/List.CSharp/List.CSharp.Tests/SimpleListTests.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace List.CSharp.Tests;
@@ -5,7 +6,13 @@ namespace List.CSharp.Tests;
 public class SimpleListTests
 {
     [Fact]
-    public void Test1()
+    public void TestOutRangeException()
     {
+        var list = new MyList.List<int>();                  // новый список int (пустой)
+        var err =
+            Record.Exception(                               // метод умеющий ловить Exception, принимает лямбда-функцию
+                () => { list.GetAt(0); }                    // безымянная функция - наз. "лямбда" (параметры тут..) = { тело функции тут...}
+            );
+        Assert.IsType<ArgumentOutOfRangeException>(err);    // библиотечная проверка что исключение именно такое как ожидается
     }
 }
